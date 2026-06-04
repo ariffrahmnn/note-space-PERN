c0c83e7e1e41c69fb93ab1173f5564d4424eca23
ariffrahmnn <ariffrahmnn1@gmail.com>
2026-06-04 15:51:30 +0700

    READYY TO POSTTTT

--- server/index.js
+++ server/index.js
@@ -13,11 +13,13 @@ import './config/passport.js';
 dotenv.config({ path: '../.env' });
 
 const app = express();
-
-app.use(cors({
-  origin: 'http://localhost:5173', 
-  credentials: true,
-}));
+const corsOptions = {
+  origin: process.env.CLIENT_URL || 'http://localhost:5173', 
+  credentials: true, 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+app.use(cors(corsOptions));
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 app.use(passport.initialize());
