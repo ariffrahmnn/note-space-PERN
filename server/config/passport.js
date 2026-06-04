aa10de042e2f373f9a229b26ac766f3ed1b70227
ariffrahmnn <ariffrahmnn1@gmail.com>
2026-06-04 16:01:25 +0700

    READYY PART 3

--- server/config/passport.js
+++ server/config/passport.js
@@ -8,7 +8,8 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: (process.env.BACKEND_URL || "http://localhost:4000") + "/api/auth/google/callback"
+    callbackURL: "https://note-space-pern-production.up.railway.app/api/auth/google/callback",
+    passReqToCallback: true  
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
