5d8fee5c23a110008ae8d906a017f478c01054a4
ariffrahmnn <ariffrahmnn1@gmail.com>
2026-06-04 16:00:14 +0700

    READYYYY PART 2

--- server/config/passport.js
+++ server/config/passport.js
@@ -8,7 +8,7 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:4000/api/auth/google/callback"
+    callbackURL: (process.env.BACKEND_URL || "http://localhost:4000") + "/api/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
